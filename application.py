f65613d0246d3d2925f99d4dc41c25cb79ce8ebf
HariniReddy4500 <114225065+HariniReddy7816@users.noreply.github.com>
2025-11-14 00:16:03 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,21 +1,4 @@
-import os
-import logging
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_session import Session
+from FlaskWebProject import app
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-# Initialize session
-Session(app)
-
-# Initialize database
-db = SQLAlchemy(app)
-
-login = LoginManager(app)
-login.login_view = 'login'
-
-import FlaskWebProject.views
+if __name__ == "__main__":
+    app.run()
