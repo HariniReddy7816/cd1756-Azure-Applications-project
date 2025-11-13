a92b090256b89d4aad0be8d090e59d8467e9a7ba
HariniReddy4500 <114225065+HariniReddy7816@users.noreply.github.com>
2025-11-13 23:08:01 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,6 +1,7 @@
 """
 The flask application package.
 """
+import os
 import logging
 from flask import Flask
 from config import Config
@@ -11,6 +12,11 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+app.config['BLOB_ACCOUNT'] = os.environ.get('BLOB_ACCOUNT')
+app.config['BLOB_STORAGE_KEY'] = os.environ.get('BLOB_STORAGE_KEY')
+app.config['BLOB_CONTAINER'] = os.environ.get('BLOB_CONTAINER')
+app.config['BLOB_CONNECTION_STRING'] = os.environ.get('BLOB_CONNECTION_STRING')
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
